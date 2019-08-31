e49758e0355bfba55ea6c0959767176411c78b72
Anwesh43 <anweshthecool0@gmail.com>
2019-08-31 13:30:51 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcoloredscreenview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcoloredscreenview/MainActivity.kt
@@ -3,12 +3,13 @@ package com.anwesh.uiprojects.linkedcoloredscreenview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.anwesh.uiprojects.coloredscreenview.ColoredScreenView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ColoredScreenView.create(this)
         fullScreen()
     }
 
