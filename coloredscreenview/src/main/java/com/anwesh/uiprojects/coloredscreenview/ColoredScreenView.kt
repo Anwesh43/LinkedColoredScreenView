0b58c5ee022c351ff9b948dfd9db4d4e772dc92f
Anwesh43 <anweshthecool0@gmail.com>
2019-08-31 12:45:22 +0530

    adding constants

--- coloredscreenview/src/main/java/com/anwesh/uiprojects/coloredscreenview/ColoredScreenView.kt
+++ coloredscreenview/src/main/java/com/anwesh/uiprojects/coloredscreenview/ColoredScreenView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.coloredscreenview
 /**
  * Created by anweshmishra on 31/08/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+
+val scGap : Float = 0.05f
+val delay : Long = 20
+val colors : Array<String> = arrayOf("#4CAF50", "#01579B", "#1A237E", "#E65100", "#f44336")
+val backColor : Int = Color.parseColor("#BDBDBD")
