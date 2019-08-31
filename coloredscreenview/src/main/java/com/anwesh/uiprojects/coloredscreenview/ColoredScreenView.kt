6225281b7a18ae1977525a0df5dffefa7fe2362a
Anwesh43 <anweshthecool0@gmail.com>
2019-08-31 12:48:59 +0530

    adding canvas extension method

--- coloredscreenview/src/main/java/com/anwesh/uiprojects/coloredscreenview/ColoredScreenView.kt
+++ coloredscreenview/src/main/java/com/anwesh/uiprojects/coloredscreenview/ColoredScreenView.kt
@@ -17,3 +17,17 @@ val scGap : Float = 0.05f
 val delay : Long = 20
 val colors : Array<String> = arrayOf("#4CAF50", "#01579B", "#1A237E", "#E65100", "#f44336")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Canvas.drawColoredScreen(i : Int, scale : Float, sc : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    var x : Float = 0f
+    if (sc != 0f) {
+        x = w * (1 - sc)
+    }
+    save()
+    translate(-w * scale + x, 0f)
+    paint.color = Color.parseColor(colors[i])
+    drawRect(RectF(0f, 0f, w, h), paint)
+    restore()
+}
